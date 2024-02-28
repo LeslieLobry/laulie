82a9cf8e9669b559a4dadee7531ff3518564da8b
LeslieLobry <leslielobry@gmail.com>
2024-02-28 18:51:31 +0100

    présentation

---
+++ src/components/Portfolio/portfolio.jsx
@@ -0,0 +1,31 @@
+import "../Portfolio/portfolio.css";
+import portfolio from "../../data/portfolio.json"
+
+function Portfolio (){
+    return(
+    <section className="portfolio">
+        { portfolio.map((product)=>{
+    return(
+    <div className="portfolio-contenant">
+        <div key={product.id}>
+        <div className="portfolio-title">
+            {product.nom}
+        </div>
+        <div className="portfolio-img">
+        <img src={product.image} alt="dev web"></img>
+        </div>
+        <div className="portfolio-descrition">
+            {product.description}
+        </div>
+        </div>
+       
+    </div>
+    )
+    })}
+    )
+
+</section>)
+
+
+}
+export default Portfolio
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,11 +1,13 @@
 import Banniere from "../../components/Banniere/banniere"
 import Presentation from "../../components/Presentation/presentation"
+import Portfolio from "../../components/Portfolio/portfolio"
 
 function Home (){
     return(
         <div className="home">
             <Banniere />
             <Presentation />
+            <Portfolio />
         </div>
     )
 }
